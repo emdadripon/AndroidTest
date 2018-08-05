8de72b32ad0bcda04f03f3ee4aa8aababef9f226
emdadripon <eriponctg@gmail.com>
2018-08-05 05:45:12 -0400

    Change

--- src/Sample/swipeTest.java
+++ src/Sample/swipeTest.java
@@ -3,6 +3,7 @@ package Sample;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
@@ -11,12 +12,17 @@ public class swipeTest extends Base{
 	public static void main(String[] args) throws MalformedURLException {
 		AndroidDriver<AndroidElement> driver = Capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		TouchAction touch = new TouchAction(driver);
          
 		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
 		driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
-		driver.findElementByXPath("//*[@content-desc='9']").click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	//	driver.findElementByXPath("//*[@content-desc='9']").click();
+		
+		touch.press(driver.findElementByXPath("//*[@content-desc='9']")).waitAction().moveTo(driver.findElementByXPath("//*[@content-desc='3']")).perform();
 	
 	
 	}
